aae9d13f1ac7ead7c858934b1e2ce03d8d323132
rrr523 <gaoyibobo@foxmail.com>
2023-04-18 13:49:48 +0800

    fix: Support boolean and number type

---
+++ eip712/greenfield/challenge/MsgSubmitSDKTypeEIP712.ts
@@ -0,0 +1,33 @@
+export const MsgSubmitSDKTypeEIP712 = {
+  "Msg": [
+    {
+      "type": "string",
+      "name": "type"
+    },
+    {
+      "type": "string",
+      "name": "challenger"
+    },
+    {
+      "type": "string",
+      "name": "sp_operator_address"
+    },
+    {
+      "type": "string",
+      "name": "bucket_name"
+    },
+    {
+      "type": "string",
+      "name": "object_name"
+    },
+    {
+      "type": "uint64",
+      "name": "segment_index"
+    },
+    {
+      "type": "bool",
+      "name": "random_index"
+    }
+  ]
+} 
+
